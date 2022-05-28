60c0c46ee1887246ce6fedcf26b1d30d3bb7ebaa
Ankit2708 <shivhare.ankit476@gmail.com>
2022-05-28 17:14:11 +0530

    expanse

---
+++ app.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const express = require('express');
+var cors = require('cors')
+
+const sequelize = require('./util/database');
+const User = require('./models/users');
+const Expense = require('./models/expenses');
+const Order = require('./models/orders');
+
+
+
+
+
+const userRoutes = require('./routes/user')
+const purchaseRoutes = require('./routes/purchase')
+
+
+const app = express();
+const dotenv = require('dotenv');
+
+// get config vars
+dotenv.config();
+
+
+app.use(cors());
+app.use(express.static('./front/Signup'))
+app.use(express.urlencoded({extended:false}))
+
+// app.use(bodyParser.urlencoded());  ////this is for handling forms
+app.use(express.json());  //this is for handling jsons
+
+app.use('/user', userRoutes)
+
+
+app.use('/purchase', purchaseRoutes)
+
+
+
+
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+
+
+sequelize.sync()
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
